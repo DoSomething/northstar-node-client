1ce24590984a95c1657637731af1fc7e00e5d10c
Sergii Tkachenko <hi@sergii.org>
2016-05-25 01:08:49 +0300

    Improve linting, fix linting errors.

--- test/northstar-node-client.test.js
+++ test/northstar-node-client.test.js
@@ -9,17 +9,12 @@ const NorthstarClient = require('../lib/northstar-node-client');
  * Test Northstar Nodejs client.
  */
 describe('NorthstarClient', () => {
-
   /* Constructor. */
   describe('constructor', () => {
-
     /* Test new instance. */
     it('should create new instance', () => {
       const client = new NorthstarClient();
       client.should.be.an.instanceof(NorthstarClient);
     });
-
   });
-
-
 });
