e95a6a8a9263f3a68ce300362e261fc2cd9842b8
Sergii Tkachenko <hi@sergii.org>
2016-06-02 16:49:03 +0300

    Test NorthstarUserAuthorized.

--- test/northstar-client.test.js
+++ test/northstar-client.test.js
@@ -6,6 +6,7 @@
 require('dotenv').config();
 const NorthstarClient = require('../lib/northstar-client');
 const NorthstarUser = require('../lib/northstar-user');
+const NorthstarUserAuthorized = require('../lib/northstar-user-authorized');
 
 /**
  * Test Northstar Nodejs client.
@@ -40,6 +41,7 @@ describe('NorthstarClient', () => {
 
       // Ensure properties and test values.
       user.should.have.properties({
+        isAuthorized: false,
         id: '5480c950bffebc651c8b456f',
         firstName: 'test',
         lastInitial: 'L',
@@ -47,7 +49,6 @@ describe('NorthstarClient', () => {
         language: 'en-global',
         country: 'US',
         drupalID: '187',
-        isAuthorized: false,
       });
 
       // Just ensure presence.
@@ -96,6 +97,61 @@ describe('NorthstarClient', () => {
 
   // With X-DS-REST-API-Key.
   describe('authorized', () => {
+    /**
+     * Helper: validate authorized user object.
+     */
+    function authorizedTestUser(user) {
+      // TODO: check to be an instance of NorthstarUser.
+      user.should.be.an.instanceof(NorthstarUser);
+      user.should.be.an.instanceof(NorthstarUserAuthorized);
+
+      // Ensure properties and test values.
+      user.should.have.properties({
+        isAuthorized: true,
+        id: '5480c950bffebc651c8b456f',
+        firstName: 'test',
+        lastName: 'Last',
+        lastInitial: 'L',
+        photo: 'https://avatar.dosomething.org/uploads/avatars/5480c950bffebc651c8b456f.jpeg',
+        email: 'test@dosomething.org',
+        mobile: '5555555555',
+        interests: [
+          'interest number 1',
+          'int num 2',
+        ],
+        birthdate: '1989-05-04 00:00:00',
+        addrStreet1: '123',
+        addrStreet2: '456',
+        addrCity: 'Paris',
+        addrState: 'Florida',
+        addrZip: '555555',
+        source: 'phoenix',
+        mobilecommonsID: null,
+        parseInstallationIds: [
+          'parse-test',
+        ],
+        mobilecommonsStatus: null,
+        language: 'en-global',
+        country: 'US',
+        drupalID: '187',
+      });
+
+      // Just ensure presence.
+      user.should.have.properties(['updatedAt', 'createdAt']);
+    }
+
+    /**
+     * Helper: test user requested with specific type.
+     */
+    function testUserBy(type, id) {
+      const client = getAuthorizedClient();
+      const response = client.getUser(type, id);
+
+      // Check response to be a Promise.
+      response.should.be.a.Promise();
+      return response.should.eventually.match(authorizedTestUser);
+    }
+
     // Constructor.
     describe('constructor', () => {
       // Check API key
@@ -109,6 +165,24 @@ describe('NorthstarClient', () => {
         client.should.be.an.instanceof(NorthstarClient);
         client.should.have.property('baseURI').which.is.not.empty();
         client.should.have.property('apiKey').which.is.not.empty();
+      });
+    });
+
+    // Get single user.
+    describe('getUser()', () => {
+      // By id.
+      it('by id should return correct Northstar user', () => {
+        testUserBy('id', '5480c950bffebc651c8b456f');
+      });
+
+      // By email.
+      it('by email should return correct Northstar user', () => {
+        testUserBy('email', 'test@dosomething.org');
+      });
+
+      // By mobile.
+      it('by mobile should return correct Northstar user', () => {
+        testUserBy('mobile', '5555555555');
       });
     });
   });
